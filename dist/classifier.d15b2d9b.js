a7fe6920c5e36c823819188112ad41b5195c9611
mathildebuenerd <mathilde.buenerd@gmail.com>
2019-01-22 17:37:15 +0100

    add css to the interface

--- dist/classifier.d15b2d9b.js
+++ dist/classifier.d15b2d9b.js
@@ -517,7 +517,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60416' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '65517' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
